00137fd14c06c9aaeeac73825399c9de435e0ba0
Vikas <61241966+viikkkas@users.noreply.github.com>
2020-11-01 12:45:04 +0530

    Update treeTraversals.cpp

--- treeTraversals.cpp
+++ treeTraversals.cpp
@@ -55,3 +55,71 @@ int main()
     postorder(root);
     return 0;
 }
+
+
+
+//..........................................//
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* left;
+    Node *right;
+    Node(int data)
+    {
+        this->data = data;
+        left = right = NULL;
+    }
+};
+
+Node* root = NULL;
+
+void insert(Node *root, int n)
+{
+    if(root==NULL)
+    {
+        Node *newNode = new Node(n);
+        cout<<"\n"<<n<<" inserted";
+    }
+    else if(n <= root->data)
+        insert(root->left,n);
+    else
+        insert(root->right,n);
+}
+
+void inorder(Node *node)
+{
+    if(node == NULL)
+        return;
+    inorder(node->left);
+    cout<<node->data;
+    inorder(node->right);
+}
+
+int main()
+{
+    char ch;
+    do
+    {
+        cout<<"\n1. Enter element in tree";
+        cout<<"\n2. Inorder Traversal";
+        cout<<"\nEnter your choice: ";
+        int n;
+        cin>>n;
+        switch(n)
+        {
+            case 1: int data;
+                    cout<<"\nEnter data: ";
+                    cin>>data;
+                    insert(root,data);
+            break;
+            case 2: cout<<"\nInorder traversal: ";
+                    inorder(root);
+            break;
+        }
+        cout<<"\nDo you want to continue? : ";
+        cin>>ch;
+    }while(ch=='y');
+}
